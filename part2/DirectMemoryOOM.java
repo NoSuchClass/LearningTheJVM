a609324743d053cc0e2c10a7f81f253ae63319f5
liuyuehe <liuyuehe09379@hellobike.com>
2019-08-05 17:23:37 +0800

    test

--- part2/DirectMemoryOOM.java
+++ part2/DirectMemoryOOM.java
@@ -5,7 +5,7 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 
 public class DirectMemoryOOM {
-    private static final int _1M = 1024 * 1024;
+    private static final int _1M = 1024 *  1024;
 
     public static void main(String[] args) throws IllegalAccessException {
         Field unsafeField = Unsafe.class.getDeclaredFields()[0];
